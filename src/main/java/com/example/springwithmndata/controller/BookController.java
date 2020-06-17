489666fa5c0bc7668519426aa674d27efb7429d1
Franz See <franz@see.net.ph>
2020-06-17 22:15:45 +0800

    Attempt to add transaction support via spring

--- src/main/java/com/example/springwithmndata/controller/BookController.java
+++ src/main/java/com/example/springwithmndata/controller/BookController.java
@@ -1,11 +1,10 @@
 package com.example.springwithmndata.controller;
 
-import com.example.springwithmndata.service.BookService;
 import com.example.springwithmndata.entity.Book;
+import com.example.springwithmndata.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-
 import java.util.List;
 
 import static com.example.springwithmndata.controller.Paths.*;
@@ -20,25 +19,21 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @ResponseBody
     @GetMapping(BOOK_GET_ONE)
     public Book getOne(@PathVariable Long id) {
         return bookService.getOne(id);
     }
 
-    @ResponseBody
     @PostMapping(BOOK_SAVE)
     public Book save(@RequestBody Book book) {
         return bookService.save(book);
     }
 
-    @ResponseBody
     @PostMapping(BOOK_BULK_SAVE)
     public List<Book> bulkSave(@RequestBody List<Book> books) {
         return bookService.bulkSave(books);
     }
-
-    @ResponseBody
+    
     @GetMapping(BOOK_FIND)
     public List<Book> find(@RequestParam String title) {
         return bookService.findAllByTitle(title);
